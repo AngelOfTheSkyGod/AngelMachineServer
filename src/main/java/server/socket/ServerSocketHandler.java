d099c58bf3b3125a599a16b491299421b85ce339
Angel <79809585+AngelOfTheSkyGod@users.noreply.github.com>
2024-06-26 21:57:43 -0500

    config check

--- src/main/java/server/socket/ServerSocketHandler.java
+++ src/main/java/server/socket/ServerSocketHandler.java
@@ -22,7 +22,7 @@ public class ServerSocketHandler extends Thread {
     public ServerSocketHandler(int portNum, ServerState serverState) throws IOException {
         port = portNum;
         System.out.println("connecting to port" + port);
-        serverSocket = new ServerSocket(8080, 0, InetAddress.getByName("76.141.228.122"));
+        serverSocket = new ServerSocket(8080, 0, InetAddress.getByName("10.0.0.205"));
         System.out.println("server socket: " + serverSocket);
     }
 
