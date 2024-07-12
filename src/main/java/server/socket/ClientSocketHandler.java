368020f7df13d299befd36cc01c10c8ba8054006
Angel <79809585+AngelOfTheSkyGod@users.noreply.github.com>
2024-07-12 17:59:57 -0500

    debug message to see if result size is size 1 or less or greater.

--- src/main/java/server/socket/ClientSocketHandler.java
+++ src/main/java/server/socket/ClientSocketHandler.java
@@ -41,7 +41,7 @@ public class ClientSocketHandler extends Thread{
                 int b;
                 while((b = in.read(buffer)) != -1){
                     result.write(buffer, 0, b);
-                    System.out.println("receiving: " +  result);
+                    System.out.println("receiving2: " +  result);
                 }
                 System.out.println(result);
 
