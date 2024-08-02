dbe7bc4e2a99a1900130edb129d81eb852e124c4
Angel <79809585+AngelOfTheSkyGod@users.noreply.github.com>
2024-08-02 18:54:58 -0500

    trimming inputs to make them able to be parsed

--- src/main/java/server/parsers/ServerObjectParser.java
+++ src/main/java/server/parsers/ServerObjectParser.java
@@ -3,22 +3,20 @@ package server.parsers;
 import server.model.ServerObject;
 
 import java.util.Optional;
-
 //  command/n
 //  username/n
 //  password
 // /r/n
 public class ServerObjectParser {
-
     public static ServerObject parse(String clientResponse, Optional<Integer> deviceNum){
 
         String[] objectFieldArray = clientResponse.split("\n");
         Integer num = deviceNum.orElse(-1);
         return ServerObject.builder()
-                .command(Integer.parseInt(objectFieldArray[0]))
+                .command(Integer.parseInt(objectFieldArray[0].trim()))
                 .deviceNum(num)
-                .username(objectFieldArray[1])
-                .password(objectFieldArray[2].replace("\r", "")).build();
+                .username(objectFieldArray[1].trim())
+                .password(objectFieldArray[2].replace("\r", "").trim()).build();
     };
 
 }
